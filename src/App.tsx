9ee2ad170ffce3e6ee9bd0a06cecb3a836ed92bc
Hecker_01 <jesseflantua@icloud.com>
2024-12-06 11:21:24 +0100

    Refactor App component to separate content logic into AppContent; enhance Header component to accept dynamic tab names and links; add Tabs component for improved navigation structure.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
 import Header from "./components/Header";
-import Sidebar from "./components/Sidebar"; // Import Sidebar
+import Sidebar from "./components/Sidebar";
 import Home from "./pages/Home";
 import Entertainment from "./pages/Entertainment";
 import Health from "./pages/Health";
@@ -11,26 +16,94 @@ import Settings from "./pages/Settings";
 const App = () => {
   return (
     <Router>
-      <div className="bg-black">
-        <Header />
-        <div className="flex flex-row flex-grow">
-          <Sidebar />
-          <main className="flex-grow flex items-center justify-center w-full">
-            <div className="bg-gray-800 text-white h-[calc(100vh-64px-3.5rem)] w-[calc(100%-0.75rem)] p-3 m-3 ml-1 mt-1 outline outline-4 outline-[#00ffff] rounded-xl">
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/entertainment" element={<Entertainment />} />
-                <Route path="/health" element={<Health />} />
-                <Route path="/travel-info" element={<TravelInfo />} />
-                <Route path="/food-drink" element={<FoodDrink />} />
-                <Route path="/settings" element={<Settings />} />
-              </Routes>
-            </div>
-          </main>
-        </div>
-      </div>
+      <AppContent />
     </Router>
   );
 };
 
+const AppContent = () => {
+  const location = useLocation();
+  const tabs = getTabs(location.pathname);
+
+  return (
+    <div className="bg-black">
+      <Header
+        name1={tabs.name1}
+        link1={tabs.link1}
+        name2={tabs.name2}
+        link2={tabs.link2}
+      />
+      <div className="flex flex-row flex-grow">
+        <Sidebar />
+        <main className="flex-grow flex items-center justify-center w-full">
+          <div className="bg-gray-800 text-white h-[calc(100vh-64px-3.5rem)] w-[calc(100%-0.75rem)] p-3 m-3 ml-1 mt-1 outline outline-4 outline-[#00ffff] rounded-xl">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/entertainment" element={<Entertainment />} />
+              <Route path="/health" element={<Health />} />
+              <Route path="/travel-info" element={<TravelInfo />} />
+              <Route path="/food-drink" element={<FoodDrink />} />
+              <Route path="/settings" element={<Settings />} />
+            </Routes>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+};
+
 export default App;
+
+function getTabs(pathname: string) {
+  switch (pathname) {
+    case "/":
+      return {
+        name1: "Home",
+        link1: "/",
+        name2: "",
+        link2: "",
+      };
+    case "/entertainment":
+      return {
+        name1: "Movies",
+        link1: "/entertainment",
+        name2: "Music",
+        link2: "/entertainment",
+      };
+    case "/health":
+      return {
+        name1: "Health",
+        link1: "/health",
+        name2: "Health",
+        link2: "/health",
+      };
+    case "/travel-info":
+      return {
+        name1: "Travel Info",
+        link1: "/travel-info",
+        name2: "Travel Info",
+        link2: "/travel-info",
+      };
+    case "/food-drink":
+      return {
+        name1: "Food & Drink",
+        link1: "/food-drink",
+        name2: "Food & Drink",
+        link2: "/food-drink",
+      };
+    case "/settings":
+      return {
+        name1: "Settings",
+        link1: "/settings",
+        name2: "Settings",
+        link2: "/settings",
+      };
+    default:
+      return {
+        name1: "Home",
+        link1: "/",
+        name2: "Home",
+        link2: "/",
+      };
+  }
+}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,32 +1,69 @@
-import { Link } from "react-router-dom";
+import { Link, To } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 
-const Header = () => {
+const Header = (tabs: {
+  name1: String;
+  link1: To;
+  name2: String;
+  link2: To;
+}) => {
   return (
-    <header className="flex flex-row bg-gray-800 text-white w-[calc(100%-1.5rem)] max-h-32 p-2 m-3 outline outline-4 outline-[#00ffff] rounded-xl">
+    <header className="flex flex-row bg-gray-800 text-white w-[calc(100%-1.5rem)] max-h-32 p-2 m-3 outline outline-4 outline-[#00ffff] rounded-xl pr-4">
       <div
         className="p-4 mr-8 flex justify-center items-center size-16 bg-slate-700
     rounded-full"
       >
         <FontAwesomeIcon icon={faUser} className="size-10" />
       </div>
-      <nav className="flex justify-between items-center p-1">
-        <ul className="flex space-x-4">
-          <li>
-            <Link to="/" className="hover:underline">
-              Tab1
-            </Link>
-          </li>
-          <li>
-            <Link to="/" className="hover:underline">
-              Tab2
-            </Link>
-          </li>
-        </ul>
+      <nav className="flex items-center p-1 w-full">
+        <Tabs
+          name1={tabs.name1}
+          link1={tabs.link1}
+          name2={tabs.name2}
+          link2={tabs.link2}
+        />
       </nav>
     </header>
   );
 };
 
 export default Header;
+
+function Tabs(tabInfo: { name1: String; link1: To; name2: String; link2: To }) {
+  if (!tabInfo.name2 || !tabInfo.link2) {
+    return (
+      <ul className="flex justify-between space-x-4 w-full">
+        <li className="tab-item w-full">
+          <Link
+            to={tabInfo.link1}
+            className="flex items-center justify-center text-center hover:underline w-full h-10"
+          >
+            <span className="text-lg font-semibold">{tabInfo.name1}</span>
+          </Link>
+        </li>
+      </ul>
+    );
+  } else {
+    return (
+      <ul className="flex justify-between space-x-4 w-full">
+        <li className="tab-item w-[49%]">
+          <Link
+            to={tabInfo.link1}
+            className="flex items-center justify-center text-center hover:underline w-full h-10"
+          >
+            <span className="text-lg font-semibold">{tabInfo.name1}</span>
+          </Link>
+        </li>
+        <li className="tab-item w-[49%]">
+          <Link
+            to={tabInfo.link2}
+            className="flex items-center justify-center text-center hover:underline w-full h-10"
+          >
+            <span className="text-lg font-semibold">{tabInfo.name2}</span>
+          </Link>
+        </li>
+      </ul>
+    );
+  }
+}
