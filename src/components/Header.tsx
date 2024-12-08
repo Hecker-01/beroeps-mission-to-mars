87c4cbdf808c92f1de2a2faa3d90f7af0da59881
Hecker_01 <jesseflantua@icloud.com>
2024-12-09 00:03:25 +0100

    Refractor code to be more efficient

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,10 +3,10 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 
 const Header = (tabs: {
-  name1: String;
+  name1: string;
   link1: To;
-  name2: String;
-  link2: To;
+  name2?: string;
+  link2?: To;
 }) => {
   return (
     <header className="flex flex-row bg-gray-800 text-white w-[calc(100%-1.5rem)] max-h-32 p-2 m-3 outline outline-4 outline-[#00ffff] rounded-xl pr-4">
@@ -17,12 +17,7 @@ const Header = (tabs: {
         <FontAwesomeIcon icon={faUser} className="size-10" />
       </div>
       <nav className="flex items-center p-1 w-full">
-        <Tabs
-          name1={tabs.name1}
-          link1={tabs.link1}
-          name2={tabs.name2}
-          link2={tabs.link2}
-        />
+        <Tabs tabs={tabs} />
       </nav>
     </header>
   );
@@ -30,43 +25,27 @@ const Header = (tabs: {
 
 export default Header;
 
-function Tabs(tabInfo: { name1: String; link1: To; name2: String; link2: To }) {
-  if (!tabInfo.name2 || !tabInfo.link2) {
-    return (
-      <ul className="flex justify-between space-x-4 w-full">
-        <li className="tab-item w-full">
-          <Link
-            to={tabInfo.link1}
-            className="flex items-center justify-center text-center hover:underline w-full h-10"
-            draggable="false"
-          >
-            <span className="text-lg font-semibold">{tabInfo.name1}</span>
-          </Link>
-        </li>
-      </ul>
-    );
-  } else {
-    return (
-      <ul className="flex justify-between space-x-4 w-full">
-        <li className="tab-item w-[49%]">
-          <Link
-            to={tabInfo.link1}
-            className="flex items-center justify-center text-center hover:underline w-full h-10"
-            draggable="false"
-          >
-            <span className="text-lg font-semibold">{tabInfo.name1}</span>
-          </Link>
-        </li>
-        <li className="tab-item w-[49%]">
-          <Link
-            to={tabInfo.link2}
-            className="flex items-center justify-center text-center hover:underline w-full h-10"
-            draggable="false"
-          >
-            <span className="text-lg font-semibold">{tabInfo.name2}</span>
-          </Link>
-        </li>
-      </ul>
-    );
-  }
+function Tabs({
+  tabs,
+}: {
+  tabs: { name1: string; link1: To; name2?: string; link2?: To };
+}) {
+  const renderTab = (name: string, link: To) => (
+    <li className="tab-item w-full">
+      <Link
+        to={link}
+        className="flex items-center justify-center text-center hover:underline w-full h-10"
+        draggable="false"
+      >
+        <span className="text-lg font-semibold">{name}</span>
+      </Link>
+    </li>
+  );
+
+  return (
+    <ul className="flex justify-between space-x-4 w-full">
+      {tabs.name1 && tabs.link1 && renderTab(tabs.name1, tabs.link1)}
+      {tabs.name2 && tabs.link2 && renderTab(tabs.name2, tabs.link2)}
+    </ul>
+  );
 }
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -10,74 +10,25 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 
 function Sidebar() {
+  const renderSidebarItem = (name: string, link: string, icon: any) => (
+    <li>
+      <Link to={link} draggable="false" className="sidebar-icon group">
+        <FontAwesomeIcon icon={icon} size="xl" />
+        <span className="sidebar-tooltip group-hover:scale-100">{name}</span>
+      </Link>
+    </li>
+  );
+
   return (
     <div className="bg-gray-800 text-white h-[calc(100vh-64px-3.5rem)] p-2 m-3 mt-1 max-w-32 outline outline-4 outline-[#00ffff] rounded-xl">
       <nav>
         <ul className="flex flex-col space-y-4 h-[calc(100vh-64px-4.5rem)] overflow-hidden">
-          <li>
-            <Link to="/" className="sidebar-icon group" draggable="false">
-              <FontAwesomeIcon icon={faHouse} size="xl" />
-              <span className="sidebar-tooltip group-hover:scale-100">
-                Home
-              </span>
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/entertainment"
-              className="sidebar-icon group"
-              draggable="false"
-            >
-              <FontAwesomeIcon icon={faFilm} size="xl" />{" "}
-              <span className="sidebar-tooltip group-hover:scale-100">
-                Entertainment
-              </span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/health" className="sidebar-icon group" draggable="false">
-              <FontAwesomeIcon icon={faHeart} size="xl" />{" "}
-              <span className="sidebar-tooltip group-hover:scale-100">
-                Health
-              </span>
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/travel-info"
-              className="sidebar-icon group"
-              draggable="false"
-            >
-              <FontAwesomeIcon icon={faCompass} size="xl" />{" "}
-              <span className="sidebar-tooltip group-hover:scale-100">
-                Travel info
-              </span>
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/food-drink"
-              className="sidebar-icon group"
-              draggable="false"
-            >
-              <FontAwesomeIcon icon={faBurger} size="xl" />{" "}
-              <span className="sidebar-tooltip group-hover:scale-100">
-                Food & Drink
-              </span>
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/settings"
-              className="sidebar-icon group"
-              draggable="false"
-            >
-              <FontAwesomeIcon icon={faSliders} size="xl" />{" "}
-              <span className="sidebar-tooltip group-hover:scale-100">
-                Settings
-              </span>
-            </Link>
-          </li>
+          {renderSidebarItem("Home", "/", faHouse)}
+          {renderSidebarItem("Entertainment", "/entertainment", faFilm)}
+          {renderSidebarItem("Health", "/health", faHeart)}
+          {renderSidebarItem("Travel info", "/travel-info", faCompass)}
+          {renderSidebarItem("Food & Drink", "/food-drink", faBurger)}
+          {renderSidebarItem("Settings", "/settings", faSliders)}
         </ul>
       </nav>
     </div>
