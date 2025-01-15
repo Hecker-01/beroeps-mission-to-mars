abc73ca5ed93a0fdea246d551808896a6424b94b
Karsten Lindenburg <102625@glr.nl>
2025-01-15 14:52:49 +0000

    dashboard pgae gemaakt met images

--- scripts/webcam.js
+++ scripts/webcam.js
@@ -1,11 +1,9 @@
-
 function changeWebcam(location) {
-  const webcamImage = document.getElementById('webcam-image');
+  const webcamImage = document.getElementById("webcam-image");
   const images = {
-    sport: './assets/Images/sport.jpg',
-    bibliotheek: './assets/Images/bibliotheek.jpg',
-    restaurant: './assets/Images/restaurant.jpg',
-    background: './assets/Images/background.png'
+    sport: "./assets/Images/sport.png",
+    bibliotheek: "./assets/Images/bibliotheek.png",
+    restaurant: "./assets/Images/restaurant.png",
   };
   webcamImage.src = images[location];
 }
